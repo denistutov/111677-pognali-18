98684143046169a7090a3d97a2741fc8a35bb925
Denis Tutov <vrednokot@gmail.com>
2020-01-24 16:16:20 +0300

    Исправление логотипа

--- source/js/page-header.js
+++ source/js/page-header.js
@@ -1,6 +1,7 @@
 var pageHeader = document.querySelector('.page-header');
 var toggleHeaderMenu = document.querySelector('.page-header__toggle');
 var headerMenu = document.querySelector('.page-header__nav');
+var headerLogo = document.querySelector('.page-header__logo');
 
 pageHeader.classList.add('page-header--js');
 
@@ -9,6 +10,12 @@ toggleHeaderMenu.addEventListener('click', function (evt) {
   pageHeader.classList.toggle('page-header--menu-open');
   headerMenu.classList.toggle('page-header__nav--is-open');
   toggleHeaderMenu.classList.toggle('page-header__toggle--is-open');
+
+  if (pageHeader.classList.contains('page-header--scroll')) {
+    evt.preventDefault();
+  } else {
+    headerLogo.classList.toggle('logo--blue');
+  }
 });
 
 window.addEventListener('scroll', function (evt) {
@@ -17,8 +24,10 @@ window.addEventListener('scroll', function (evt) {
   } else {
     if (window.pageYOffset > 300) {
       pageHeader.classList.add('page-header--scroll');
+      headerLogo.classList.add('logo--blue');
     } else {
       pageHeader.classList.remove('page-header--scroll');
+      headerLogo.classList.remove('logo--blue');
     }
   }
 });
